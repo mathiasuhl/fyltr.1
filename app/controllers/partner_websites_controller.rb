ee3fd7f9707afc5f0f6af6ce0a589ae817f1553e
Prelang Builder <builder@prelang.com>
2016-03-10 22:14:54 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/partner_websites_controller.rb
+++ app/controllers/partner_websites_controller.rb
@@ -1,4 +1,8 @@
 class PartnerWebsitesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_partner_website, only: [:show, :edit, :update, :destroy]
 
   # GET /partner_websites
